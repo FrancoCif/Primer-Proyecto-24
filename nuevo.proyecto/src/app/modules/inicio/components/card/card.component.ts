a207fdb8eb96655f1ddf08a9594615f09361bd22
FrancoCif <rocozancho@gmail.com>
2024-05-06 09:27:32 -0300

    configuraciones de interfaz, uso de directivas e interpolación

--- nuevo.proyecto/src/app/modules/inicio/components/card/card.component.ts
+++ nuevo.proyecto/src/app/modules/inicio/components/card/card.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+
+//IMPORTAMOS INTERFAZ
+import { Zapatilla } from 'src/app/models/zapatilla';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+
+//propiedad publica tipo array
+public info: Zapatilla[]
+
+constructor() {
+  this.info = [
+    {
+      id: "",
+      nombre: "Puma",
+      modelo: "Suede XL x Pleasures",
+      año: 2023,
+      talle: 11,
+      imagen: "https://highxtar.com/wp-content/uploads/2024/01/thumb-puma-x-pleasures-suede-xl-1440x1080.jpg",
+      alt: "Puma Suede XL x Pleasures",
+    },
+    {
+      id: "",
+      nombre: "Adidas",
+      modelo: "Campus 00s",
+      año: 2023,
+      talle: 11,
+      imagen: "https://www.copncop.com/img/cms/adidas-campus-00s-red-copncop-blog.jpg",
+      alt: "Campus 00s",
+    },
+     
+  ]
+}
 
 }
