0bc9b6f96992990d6c4b1902284b6d9387880582
FrancoCif <rocozancho@gmail.com>
2024-06-05 11:08:28 -0300

    dejamos vinculada la base con nuestro proyecto

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment={
+    production: false,
+//sacamos la palabra const y cmabamos el igual por dos puntitos
+    firebaseConfig : {
+        apiKey: "AIzaSyDsZWjELBv0gTKajoNWa0tXIFJ19OtGUXc",
+        authDomain: "programacion-1612.firebaseapp.com",
+        projectId: "programacion-1612",
+        storageBucket: "programacion-1612.appspot.com",
+        messagingSenderId: "809865164502",
+        appId: "1:809865164502:web:27dce807a676b0b0e73b77",
+        measurementId: "G-LT3C41KVNV"
+      }
+}
